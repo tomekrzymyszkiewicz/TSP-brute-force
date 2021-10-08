735d0a059c7ae48afb233e7ce130f1536c92c9f8
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-10-08 16:11:29 +0200

    loading config

--- main.cpp
+++ main.cpp
@@ -9,43 +9,42 @@
 #include <random>
 #include <string>
 #include <windows.h>
+#include <sstream>
+#include <iterator>
 #include "structures.h"
 using namespace std;
 using namespace std::chrono;
 
 vector<vector<string>> tasks;
 vector<string> results;
-string data_file_name = "";
 string results_file_name = "";
 vector<node> graph_data = vector<node>();
 int number_of_current_graph_vertices = 0;
 int number_of_current_graph_edges = 0;
+adjacency_matrix current_graph_adjacency_matrix = adjacency_matrix();
 
 
 struct Result
 {
-    string structure;
-    string operation;
-    string graph_type;
-    int number_of_vertices;
-    int number_of_edges;
-    double time_memory;
+    string graph_name;
+    int* path;
+    double time;
     int number_of_repeats;
-    string test_type;
-    Result(string structure, string graph_type, string operation, int number_of_vertices, int number_of_edges, double time_memory, int number_of_repeats, string test_type)
+    Result(string graph_name, int* path, double time, int number_of_repeats)
     {
-        this->structure = structure;
-        this->operation = operation;
-        this->graph_type = graph_type;
-        this->number_of_vertices = number_of_vertices;
-        this->number_of_edges = number_of_edges;
-        this->time_memory = time_memory;
+        this->graph_name = graph_name;
+        this->path = path;
+        this->time = time;
         this->number_of_repeats = number_of_repeats;
-        this->test_type = test_type;
     }
     string toString()
-    {
-        return (structure + "," + graph_type + "," + operation + "," + to_string(number_of_vertices) + "," + to_string(number_of_edges) + "," + to_string(time_memory) + "," + to_string(number_of_repeats) + "," + test_type);
+    {   
+        string path_string = "";
+        for(int i = 0; i < number_of_current_graph_vertices; i++){
+            path_string += path[i];
+            path_string += " ";
+        }
+        return (graph_name + "," + path_string + "," + to_string(time) + "," + to_string(number_of_repeats));
     }
 };
 
@@ -54,13 +53,27 @@ void save_results(string results_file_name)
     std::cout << "Saving results" << endl;
     fstream fout;
     fout.open(results_file_name, ios::out);
-    fout << "structure,graph_type,operation,number_of_vertices,number_of_edges,time_memory,number_of_repeats,test_type" << endl;
+    fout << "graph_name,path,time,number_of_repeats" << endl;
     for (int i = 0; i < results.size(); i++)
     {
         fout << results[i] << endl;
     }
     fout.close();
     std::cout << "Correctly saved " << results.size() << " results" << endl;
+}
+
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
 }
 
 bool load_data(string file_name)
@@ -76,18 +89,14 @@ bool load_data(string file_name)
     }
     string loaded_source, loaded_destination, loaded_weight;
     int edges_loaded = 0;
-    number_of_current_graph_vertices = 0;
-    number_of_current_graph_edges = 0;
-    while (!fin.eof())
-    {
-        fin >> loaded_source >> loaded_destination >> loaded_weight;
-        if (!fin.eof())
-        {
-            graph_data.push_back(node(stoi(loaded_source), stoi(loaded_destination), stoi(loaded_weight)));
-            number_of_current_graph_edges += 1;
-            if (number_of_current_graph_vertices < stoi(loaded_destination) + 1)
-                number_of_current_graph_vertices = stoi(loaded_destination) + 1;
-            edges_loaded++;
+    fin >> number_of_current_graph_vertices;
+    current_graph_adjacency_matrix = adjacency_matrix(number_of_current_graph_vertices);
+    for(int i = 0; i < number_of_current_graph_vertices-1; i++){
+        string loaded_line_of_matrix = "";
+        getline(fin,loaded_line_of_matrix);
+        vector<string> single_line = split(loaded_line_of_matrix,' ');
+        for(int j = 0; j < single_line.size(); j++){
+            current_graph_adjacency_matrix.add_edge_undir(i,j,stoi(single_line[j]));
         }
     }
     std::cout << "Loaded correctly " << edges_loaded << " edges and " << number_of_current_graph_vertices << " vertices" << endl;
@@ -106,25 +115,31 @@ void load_config()
         fin.close();
         return;
     }
-    vector<string> row;
-    string line;
-    fin >> results_file_name;
-    while (!fin.eof())
+    string loaded_line_of_task = "";
+    getline(fin,results_file_name);
+    while (getline(fin,loaded_line_of_task))
     {
-        string file_name, structure, graph_type, operation, number_of_repeats, test_type;
-        fin >> file_name >> structure >> graph_type >> operation >> number_of_repeats >> test_type;
-        if (file_name.size() == 0 || structure.size() == 0 || graph_type.size() == 0 || operation.size() == 0 || number_of_repeats.size() == 0 || test_type.size() == 0)
+        vector<string> single_line = split(loaded_line_of_task,' ');
+        string graph_file_name, number_of_repeats, shortest_path_weight, shortest_path;
+        if(single_line.size() >= 4){
+            graph_file_name = single_line[0];
+            number_of_repeats = single_line[1];
+            shortest_path_weight = single_line[2];
+            for(int i = 3; i < single_line.size(); i++){
+                shortest_path += single_line[i];
+                shortest_path += " ";
+            }
+        }
+        if (graph_file_name.size() == 0 || number_of_repeats.size() == 0 || shortest_path_weight.size() == 0 || shortest_path.size() == 0)
         {
-            std::cout << "Cannot load this task: " << structure << graph_type << operation << number_of_repeats << test_type;
+            std::cout << "Cannot load this task: " << graph_file_name <<" "<< number_of_repeats <<" "<< shortest_path_weight <<" "<< shortest_path;
             break;
         }
         vector<string> task;
-        task.push_back(file_name);
-        task.push_back(structure);
-        task.push_back(graph_type);
-        task.push_back(operation);
+        task.push_back(graph_file_name);
         task.push_back(number_of_repeats);
-        task.push_back(test_type);
+        task.push_back(shortest_path_weight);
+        task.push_back(shortest_path);
         tasks.push_back(task);
     }
     fin.close();
@@ -145,16 +160,14 @@ int main()
         {
             std::cout << endl;
             string graph_file_name = tasks[i][0];
-            string structure = tasks[i][1];
-            string graph_type = tasks[i][2];
-            string operation = tasks[i][3];
-            int number_of_repeats = stoi(tasks[i][4]);
-            string test_type = tasks[i][5];
+            int number_of_repeats = stoi(tasks[i][1]);
+            string shortest_path_weight = tasks[i][2];
+            string shortest_path = tasks[i][3];
             if (!load_data(graph_file_name))
             {
-                std::cout << "Cannot load graph from " << data_file_name << " file." << endl;
+                std::cout << "Cannot load graph from " << graph_file_name << " file." << endl;
             }
-            std::cout << "Computing " << operation << " in " << structure << " graph with " << number_of_current_graph_edges << " edges and " << number_of_current_graph_vertices << " vertices repeated " << number_of_repeats << " times; " << test_type << " test" << endl;
+            std::cout << "Computing TSP in " << graph_file_name << " graph with " << number_of_current_graph_edges << " edges and " << number_of_current_graph_vertices << " vertices repeated " << number_of_repeats << " times" << endl;
             if (number_of_current_graph_edges < 1)
             {
                 std::cout << "Cannot execute task. The array must to have at least 1 element.";
